a0120a88b7466c3a5c7fcbc962d88ea9bde5a7e7
Vivek <230504152+geekyvivekbytes@users.noreply.github.com>
2025-12-04 11:56:04 +0530

    day 4 completed

--- year_2019/day4/day4.js
+++ year_2019/day4/day4.js
@@ -1,2 +1,51 @@
-const input = 146810 - 612564.;
-console.log(input);
+const start = 146810;
+const limit = 612564.;
+// const a = Array.from({ length: 612564 - 146810 }, () => start + i++);
+
+const splitedNum = (num) => {
+  let number = num;
+  const array = [];
+  for (let i = 0; i < 6; i++) {
+    array.unshift(number % 10);
+    number = (number - number % 10) / 10;
+  }
+  return array;
+};
+
+const isValidPassword = (num) => {
+  const array = splitedNum(num);
+  for (let i = 1; i < array.length; i++) {
+    if (!(array[i] >= array[i - 1])) {
+      return false;
+    }
+  }
+  return array.some((x, i) => (x === array[i + 1]));
+};
+
+const isValidPassword2 = (num) => {
+  const array = splitedNum(num);
+  for (let i = 1; i < array.length; i++) {
+    if (!(array[i] >= array[i - 1])) {
+      return false;
+    }
+  }
+  return array.some((x, i) =>
+    array.lastIndexOf(x) - i === 1 && array.indexOf(x) === i
+  );
+};
+
+let count = 0;
+let count2 = 0;
+for (let i = start; i <= limit; i++) {
+  if (isValidPassword2(i)) {
+    count2++;
+  }
+}
+
+for (let i = start; i <= limit; i++) {
+  if (isValidPassword(i)) {
+    count++;
+  }
+}
+console.log("password1", count);
+console.log("password12", count2);
